00234b6cba2d30154ad77a4343d64c780c2326da
kimminseoung <ms930610@naver.com>
2022-09-13 17:15:43 +0900

    2022-09-13

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
@@ -12,9 +12,9 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
-        </HashRouter>
+        </BrowserRouter>
       </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>
